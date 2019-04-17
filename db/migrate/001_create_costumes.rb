954b7386ec8e056ce481030a50323b3274d1570d
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-17 21:35:11 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,7 +13,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
     create_table :costumes do |t|
       t.string :name
       t.float :price
-      t.string :price
+      t.string :size
       t.string :image_url
       t.datetime :created_at
       t.datetime :updated_at
