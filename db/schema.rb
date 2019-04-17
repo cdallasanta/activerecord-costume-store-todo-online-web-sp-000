c7eef17a1c34fdb2c6efcbee036d6e3883178eb0
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-17 21:37:02 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -24,10 +24,11 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
-    t.string "price"
+    t.float "price"
+    t.string "size"
     t.string "image_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "haunted_houses", force: :cascade do |t|
