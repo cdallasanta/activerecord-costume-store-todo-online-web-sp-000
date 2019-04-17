fa79963c6cd24cef1f5b34cb38d7b751eaa91c34
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-17 21:37:25 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "costume_stores", force: :cascade do |t|
     t.string "name"
@@ -24,22 +24,10 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
-    t.float "price"
-    t.string "size"
+    t.string "price"
     t.string "image_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "haunted_houses", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.string "theme"
-    t.float "price"
-    t.boolean "family_friendly"
-    t.date "opening_date"
-    t.date "closing_date"
-    t.string "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
